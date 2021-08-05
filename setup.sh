22e914d3d6e498c58892531aa51471f54200c4d0
Vlad Ivanovic <vladivanovic@gmail.com>
2021-08-05 11:28:46 +0900

    fixed setup.sh to include tmux for now

--- setup.sh
+++ setup.sh
@@ -8,14 +8,14 @@ source /etc/os-release
 if [ $ID == "ubuntu" ]; then
 	sudo apt-get update
 	sudo apt-get upgrade
-	sudo apt-get install python3 python3-pip -y
+	sudo apt-get install python3 python3-pip tmux -y
 	sudo snap install ngrok
 	pip3 install meraki flask pyngrok
 elif [ $ID == "centos" ]; then
 	sudo yum update
 	sudo yum upgrade
 	# NEED TO VERIFY THIS PART
-	sudo yum install python3 python3-pip ngrok-client
+	sudo yum install python3 python3-pip ngrok-client tmux -y
 	pip3 install meraki flask pyngrok
 else
 	echo $ID
