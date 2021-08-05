860b967b1c2b172ed05fa1c94f437ca3342e8ed3
Vlad Ivanovic <vladivanovic@gmail.com>
2021-08-05 16:53:51 +0900

    kicked off engineio.py with port 5002, updated the secret status pages /status on each as well

--- app/engineio.py
+++ app/engineio.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 # All of the Imports
 from flask import Flask, render_template, request, Response, jsonify
-from pyngrok import ngrok
 import app_startchecks as appsc
 
 # Kickstart Flask App
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fred1234'  # (temporary random string)
-
-appsc.startngroktunnel()
+app.config['SECRET_KEY'] = 'engineio1234'  # (temporary random string)
 
 
 # Default App Route and Webhook Status
 @app.route('/')
 def index():
-    ngrok_tunnels = ngrok.get_tunnels()
-    ngrok_tunnel = []
-    if ngrok_tunnels is not None:
-        for tunnels in ngrok_tunnels:
-            tunnels = str(tunnels)
-            ngrok_tunnel.append(tunnels)
-            print(ngrok_tunnel)
-        return jsonify(status='TUNNEL UP', tunnel_data=ngrok_tunnel)
-    else:
-        return jsonify(status='TUNNEL DOWN')
+    return Response(status=200)
 
 
 # Create Webhook Listener
@@ -40,4 +28,4 @@ def status():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=5002, debug=False)
