f538527d1b68f11fa311f727e8b44b22fc501b0b
pandaxsunshine@gmail.com <pandaxsunshine@gmail.com>
2021-08-17 01:10:47 +1000

    update credentials.json and loginScript.py

--- apimodules/login/loginScript.py
+++ apimodules/login/loginScript.py
@@ -1,23 +1,17 @@
 #login script with prompt?
 
-import requests
-import os
 import json
 
 ## declaration and initialization
 
-dir = os.path.abspath(os.path.dirname(__file__))
+creds = dict()
 
-with open(os.path.join(dir, "login/credentials.json")) as temp:
-    creds = temp.read()
+creds["APIKey"] = input("Enter Meraki Dashboard API Key:\n")
+
+creds["netID"] = input("Enter Network ID:\n")
 
 
-jsonCreds = json.loads(creds)
 
-APIKey = str(jsonCreds["APIKey"])
 
-payload = {}
-
-headers = {
-  'X-Cisco-Meraki-API-Key': APIKey
-}
+#test display output
+print(creds)
